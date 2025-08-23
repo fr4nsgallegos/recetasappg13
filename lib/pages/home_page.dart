398dc540c168b2dd7ecd319c70c90a72d41075a1
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-08-23 11:21:26 -0500

    texts fields para recetas

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:recetasappg13/widgets/form_item_widget.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _preparationController = TextEditingController();
+  TextEditingController _imageController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsetsGeometry.all(32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FormItemWidget(
+                controller: _titleController,
+                hint: "Ingresa el título",
+                iconData: Icons.title,
+              ),
+              FormItemWidget(
+                controller: _preparationController,
+                hint: "Ingrsa la receta",
+                iconData: Icons.list,
+                maxLines: 3,
+              ),
+              FormItemWidget(
+                controller: _imageController,
+                hint: "Ingresa el url de la imagen",
+                iconData: Icons.image,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                },
+                child: Text("DEGUG"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/form_item_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class FormItemWidget extends StatelessWidget {
+  TextEditingController controller;
+  String hint;
+  IconData iconData;
+  int maxLines;
+
+  FormItemWidget({
+    required this.controller,
+    required this.hint,
+    required this.iconData,
+    this.maxLines = 1,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: TextField(
+        controller: controller,
+        style: TextStyle(color: Colors.white),
+        maxLines: maxLines,
+        decoration: InputDecoration(
+          prefixIcon: Icon(iconData),
+          prefixIconColor: Colors.white,
+          hintText: hint,
+          hintStyle: TextStyle(color: Colors.white),
+          filled: true,
+          fillColor: Color(0xff0f4c5c).withOpacity(0.8),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide.none,
+          ),
+        ),
+      ),
+    );
+  }
+}
