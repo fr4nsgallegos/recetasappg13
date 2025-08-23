68ddc8583e23f55ac024eb3df204556f0a29eb32
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-08-23 09:33:00 -0500

    dispose de variable

--- lib/examples/textfield_page.dart
+++ lib/examples/textfield_page.dart
@@ -17,6 +17,13 @@ class _TextfieldPageState extends State<TextfieldPage> {
   }
 
   @override
+  void dispose() {
+    _correoController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Text Field Page"), centerTitle: true),
