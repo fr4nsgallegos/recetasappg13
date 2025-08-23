8d1de6b54b124c0b2b33f5b4ea8ae8e4790b71aa
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-08-23 12:35:49 -0500

    lsitview.builder

--- lib/pages/scroll_page.dart
+++ lib/pages/scroll_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class ScrollPage extends StatelessWidget {
-  Widget _buildContainer() {
+  Widget _buildContainer(int number) {
     return Container(
       width: 300,
       height: 300,
       color: Colors.red,
       margin: EdgeInsets.all(16),
+      child: Center(
+        child: Text(
+          number.toString(),
+          style: TextStyle(fontSize: 50, color: Colors.white),
+        ),
+      ),
     );
   }
 
@@ -14,16 +20,33 @@ class ScrollPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Scroll Page")),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          children: [
-            _buildContainer(),
-            _buildContainer(),
-            _buildContainer(),
-            _buildContainer(),
-          ],
-        ),
+      body: Column(
+        children: [
+          Text("Texto estático sin scroll"),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 200,
+              itemBuilder: (BuildContext context, int index) {
+                print(index);
+                return _buildContainer(index);
+              },
+            ),
+          ),
+
+          // Expanded(
+          //   child: SingleChildScrollView(
+          //     scrollDirection: Axis.vertical,
+          //     child: Column(
+          //       children: [
+          //         _buildContainer(),
+          //         _buildContainer(),
+          //         _buildContainer(),
+          //         _buildContainer(),
+          //       ],
+          //     ),
+          //   ),
+          // ),
+        ],
       ),
     );
   }
