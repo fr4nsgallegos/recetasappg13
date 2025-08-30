17e47fdcb4ae5e1a72fed96b652e1a4bcd032143
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-08-29 20:24:15 -0500

    enviar y recibir ingo entre pantallas

--- lib/pages/navegacion/navigator/navigator_page.dart
+++ lib/pages/navegacion/navigator/navigator_page.dart
@@ -9,7 +9,11 @@ class NavigatorPage extends StatelessWidget {
       // Ruta inical
       initialRoute: "/",
       // Mapa de rutas nombradas
-      routes: {"/": (_) => HomeNavigatorPage()},
+      routes: {
+        "/": (_) => HomeNavigatorPage(),
+        "/detail": (_) => DetailPage(detalleObligatorio: "Obligatorio"),
+        "/about": (_) => AboutPage(),
+      },
     );
   }
 }
@@ -19,17 +23,72 @@ class HomeNavigatorPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Home Navigator Page")),
-      body: Center(child: Column()),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // 1. ir al detail page con argumentos y esperar un resultado
+            ElevatedButton(
+              onPressed: () async {
+                // enviamos un argumen - puede ser cualquier objeto
+                final result = await Navigator.pushNamed(
+                  context,
+                  "/detail",
+                  arguments: "Hola deslde el Home",
+                );
+
+                // Al volver de Detail, mostramos el resultado - si es que se devolvió algo
+                if (context.mounted && result != null) {
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(SnackBar(content: Text("Resultado: $result")));
+                }
+              },
+              child: Text("pushnamed a /detail con argumentos"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
 class DetailPage extends StatelessWidget {
-  const DetailPage({super.key});
+  String detalleObligatorio;
+
+  DetailPage({super.key, required this.detalleObligatorio});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // Recuperamos el argumeno enviado desde el Home
+    final args = ModalRoute.of(context)!.settings.arguments as String?;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detail Page"),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context, "OK DESDE EL DETAIL (appbar)");
+          },
+          icon: Icon(Icons.arrow_back),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(args ?? "Sin argumentos"),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Regresar al Home, enviado un "Resultado"
+                Navigator.pop(context, "OK DESDE EL DETAIL (body)");
+              },
+              child: Text("Pop con resultado"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
