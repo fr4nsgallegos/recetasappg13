395c4b67ac941d3d23f830f65fea122bcb616f2c
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-08-29 21:28:32 -0500

    devolveinedo resultado

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:recetasappg13/examples/textfield_page.dart';
 import 'package:recetasappg13/examples/textformfield_page.dart';
 import 'package:recetasappg13/pages/home_page.dart';
-import 'package:recetasappg13/pages/navegacion/navigator/navigator_page.dart';
+import 'package:recetasappg13/pages/navegacion/gorouter_page.dart';
+import 'package:recetasappg13/pages/navegacion/navigator_page.dart';
 import 'package:recetasappg13/pages/scroll_page.dart';
 import 'package:recetasappg13/pages/sliver/sliver_page.dart';
 
 void main() {
-  runApp(MaterialApp(home: NavigatorPage(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: GorouterPage(), debugShowCheckedModeBanner: false));
 }
---
+++ lib/pages/navegacion/gorouter_page.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class GorouterPage extends StatelessWidget {
+  const GorouterPage({super.key});
+
+  // Configurar el GOrouter
+  static final GoRouter _router = GoRouter(
+    routes: [
+      // Ruta raiz
+      GoRoute(
+        path: "/",
+        name: "home",
+        builder: (context, state) => HomeRouterPage(),
+      ),
+
+      // Ruta con parametro id y query parameter opcional(?from=...)
+      GoRoute(
+        path: "/detail/:id",
+        name: "detail",
+        builder: (context, state) {
+          // path parametro obligatorio
+          final id = state.pathParameters["id"]!;
+
+          // parametro query opcional
+          final from = state.uri.queryParameters["from"];
+          return DetailRouterPage(id: id, from: from);
+        },
+      ),
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: "GOROUTER DEMO",
+      routerConfig: _router,
+    );
+  }
+}
+
+class HomeRouterPage extends StatelessWidget {
+  const HomeRouterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home gorouterpage")),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // 1. Navegar con push con path + query y esperar un resultado
+            ElevatedButton(
+              onPressed: () async {
+                final result = await context.push("/detail/123?from=home");
+                if (context.mounted && result != null) {
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(SnackBar(content: Text("resultado: $result")));
+                }
+              },
+              child: Text("Push a /detail/123? from=home"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DetailRouterPage extends StatelessWidget {
+  String id;
+  String? from;
+  DetailRouterPage({required this.id, this.from});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Detail page")),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("id : $id"),
+            Text("from: ${from ?? "-"}"),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                context.pop("OK desde detailpage");
+              },
+              child: Text("POP CON REULSTADO"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AboutRouterPage extends StatelessWidget {
+  const AboutRouterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
