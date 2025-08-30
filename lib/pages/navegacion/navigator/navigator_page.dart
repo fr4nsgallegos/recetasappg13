26c8b85eff505d231b1bdebec0ea82676150ea09
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-08-29 20:05:51 -0500

    segundo ejemplo mas simple navigator

--- lib/pages/navegacion/navigator/navigator_page.dart
+++ lib/pages/navegacion/navigator/navigator_page.dart
@@ -4,102 +4,49 @@ class NavigatorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {
-        '/': (_) => HomeNavigatorPage(),
-        //ruta raiz
-      },
-      onGenerateRoute: (settings) {
-        // Centralizar las rutas con argumentos
-        if (settings.name == "/product") {
-          final args =
-              settings.arguments as Map<String, dynamic>?; //puede ser null
-          final id = (args?["id"]) as int? ?? 0;
-          return MaterialPageRoute(
-            builder: (context) => ProductNavigatorPage(id: id),
-            settings: settings,
-            fullscreenDialog: false,
-          );
-        }
-        ;
-        // 404 genérico
-        return MaterialPageRoute(builder: (context) => NotFoundNavigatorPage());
-      },
+      debugShowCheckedModeBanner: false,
+      title: "NAVIGATOR DEMO",
+      // Ruta inical
+      initialRoute: "/",
+      // Mapa de rutas nombradas
+      routes: {"/": (_) => HomeNavigatorPage()},
     );
   }
 }
 
 class HomeNavigatorPage extends StatelessWidget {
-  Future<void> _openProduct(BuildContext context) async {
-    // pushnamed con argumentos
-    final result = await Navigator.of(
-      context,
-    ).pushNamed("/product", arguments: {"id": 99});
-
-    if (!context.mounted) return;
-    if (result != null) {
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(SnackBar(content: Text("Resultado $result")));
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Navigator")),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () => _openProduct(context),
-              child: Text("Ir al producto 99 y espperar el resultado"),
-            ),
-          ],
-        ),
-      ),
+      appBar: AppBar(title: Text("Home Navigator Page")),
+      body: Center(child: Column()),
     );
   }
 }
 
-class ProductNavigatorPage extends StatelessWidget {
-  final int id;
-  const ProductNavigatorPage({super.key, required this.id});
+class DetailPage extends StatelessWidget {
+  const DetailPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Producto $id")),
-      body: Center(
-        child: Column(
-          children: [
-            Text("Mostrando el detalle del producto $id"),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop("OK-$id");
-              },
-              child: Text("Volver con resultado"),
-            ),
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
 
-class LoogedInNavigatorPage extends StatelessWidget {
-  const LoogedInNavigatorPage({super.key});
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Sesión iniciada")));
+    return const Placeholder();
   }
 }
 
-class NotFoundNavigatorPage extends StatelessWidget {
-  const NotFoundNavigatorPage({super.key});
+class InlinePage extends StatelessWidget {
+  const InlinePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("No encontrado")));
+    return const Placeholder();
   }
 }
